c564003cf3dedd121cee59fcaaf374190ec7e316
jahnavisinghal <jahnavi.singhal@gmail.com>
2017-01-16 02:43:09 +0530

    Update guess_number_higher_lower.cpp

--- leetcode/guess_number_higher_lower.cpp
+++ leetcode/guess_number_higher_lower.cpp
@@ -1,4 +1,7 @@
-nt guess(int num);
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
 
 class Solution {
 public:
